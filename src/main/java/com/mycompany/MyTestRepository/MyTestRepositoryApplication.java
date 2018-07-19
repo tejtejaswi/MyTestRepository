4f7d698804233f5630e585a74d669f04d22fd3e9
AIUCUJ3 <AIUCUJ3@USWSA035579.na.intranet.msd>
2018-07-19 13:13:14 -0400

    Added Spring Boot java commit

---
+++ src/main/java/com/mycompany/MyTestRepository/MyTestRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.MyTestRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestRepositoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mycompany/MyTestRepository/MyTestRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.MyTestRepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTestRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
